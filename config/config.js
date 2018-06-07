61508434c1ac3a8fc3ac9d4f486eb7e11c0cd640
David Cho <david.hoontime@gmail.com>
2018-06-07 22:37:58 +0200

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,5 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+module.exports = {
+  db: 'mongodb+srv://hoontime:hoontime86@db-webtech-mx3hz.mongodb.net/restaurant_aggregator?retryWrites=true',
+  db_dev: 'mongodb://localhost:27017/restaurant_aggregator',
+};
