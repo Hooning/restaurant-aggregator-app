15c3b3e5fc66e438e4898ecbfd629cf3b0420ed3
David Cho <david.hoontime@gmail.com>
2018-06-30 00:46:01 +0200

    delete: img / css / js not in use

--- client/app/index.js
+++ client/app/index.js
@@ -14,13 +14,6 @@ import './styles/styles.scss';
 import './vendor/bootstrap/css/bootstrap.min.css';
 import './fonts/font-awesome-4.7.0/css/font-awesome.min.css';
 import './fonts/themify/themify-icons.css';
-import './vendor/animate/animate.css';
-import './vendor/css-hamburgers/hamburgers.min.css';
-import './vendor/animsition/css/animsition.min.css';
-import './vendor/select2/select2.min.css';
-import './vendor/daterangepicker/daterangepicker.css';
-import './vendor/slick/slick.css';
-import './vendor/lightbox2/css/lightbox.min.css';
 import './css/util.css';
 import './css/main.css';
 
